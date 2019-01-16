1d32692dd6ccddd0e69f5dcfe1f9f1f592c160ba
dmitry <d.karpushkin@pixelplex.io>
2019-01-16 19:47:40 +0300

    fix

--- src/constants/RoundConstants.js
+++ src/constants/RoundConstants.js
@@ -4,10 +4,15 @@ export const GC_STARTED = 'gc_started';
 export const BBA_STARTED = 'bba_started';
 export const DONE = 'done';
 
+export const PRODUCING_TIP = 'On this stage, the system choose accounts who will propose the next block and wait for this block from selected producers';
+export const GC_TIP = 'This is the first round of consensus - Graded Consensus. Nodes who were chosen by the system for verifying block choose the best candidate';
+export const BBA_TIP = 'Chosen verifiers should reach an agreement to apply the new block';
+
 export const rounderSteps = {
 	[ROUND_STARTED]: {
 		step: 0,
 		progress: 30,
+		maxProgress: 49,
 		producing: 'progress',
 		verifying: 'progress',
 		timer: true,
@@ -18,12 +23,14 @@ export const rounderSteps = {
 	[GC_STARTED]: {
 		step: 2,
 		progress: 50,
+		maxProgress: 69,
 		producing: 'done',
 		verifying: 'progress',
 	},
 	[BBA_STARTED]: {
 		step: 3,
-		progress: 80,
+		progress: 70,
+		maxProgress: 99,
 		producing: 'done',
 		verifying: 'done',
 	},
