247366f6b87cddd2b65432ff6c0377a1a26fe435
a.rudenko <a.rudenko@pixelplex.io>
2019-01-22 15:52:56 +0300

    add new styles

--- src/components/TransactionInfo/index.jsx
+++ src/components/TransactionInfo/index.jsx
@@ -5,11 +5,21 @@ import { Link } from 'react-router-dom';
 import copy from 'copy-to-clipboard';
 
 import { INDEX_PATH, BLOCK_INFORMATION_PATH } from '../../constants/RouterConstants';
+import { BYTECODE_SYMBOLS_LENGTH } from '../../constants/GlobalConstants';
+
 import FormatHelper from '../../helpers/FormatHelper';
 import URLHelper from '../../helpers/URLHelper';
 import BreadCrumbs from '../../components/InformationBreadCrumbs';
 
 class TransactionsInfo extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			loadMore: props.operations.map(() => false),
+		};
+	}
 
 	componentDidMount() {
 		this.props.getTransaction();
@@ -17,6 +27,14 @@ class TransactionsInfo extends React.Component {
 
 	componentWillUnmount() {
 		this.props.clearTransaction();
+	}
+
+	onToggleLoadMore(index, e) {
+		e.preventDefault();
+
+		const { loadMore } = this.state;
+		loadMore[index] = !loadMore[index];
+		this.setState({ loadMore });
 	}
 
 	returnFunction() {
@@ -31,7 +49,11 @@ class TransactionsInfo extends React.Component {
 						<span key={log.data}>
 							<span><b>Log [{index}]</b>:<br /></span>
 							<span>
-								<span><b>Contract: </b></span><span>{log.contract}<br /></span>
+								<span><b>Contract: </b></span>
+								<span>
+									<Link to={URLHelper.createUrlById(log.contract)} className="blue">{log.contract}</Link>
+									<br />
+								</span>
 								<span><b>Topics</b><br /></span>
 								{log.topics.map((topic, i) => (<span key={topic}>[{i}]: {topic}<br /></span>))}
 								<span><b>Data</b><br /></span>
@@ -45,7 +67,7 @@ class TransactionsInfo extends React.Component {
 		);
 	}
 
-	renderOperationRow(key, value, opKey) {
+	renderOperationRowValue(key, value, index) {
 		if (typeof value === 'object' && key !== 'logs') {
 			if (!value.link) {
 				value = FormatHelper.formatAmount(value.amount, value.precision, value.symbol);
@@ -66,14 +88,30 @@ class TransactionsInfo extends React.Component {
 			value = FormatHelper.formatAmount(value, 0);
 		}
 
+		if (key === 'logs') {
+			return this.renderContractLogs(value);
+		}
+
+		if (key !== 'bytecode') {
+			return value;
+		}
+
+		const { loadMore } = this.state;
+
+		const bytecode = value.length > BYTECODE_SYMBOLS_LENGTH && !loadMore[index] ?
+			value.slice(0, BYTECODE_SYMBOLS_LENGTH - 3).concat('...') : value;
+
 		return (
-			<div className={classnames('row', { bytecode: ['bytecode', 'logs'].includes(key) })} key={`${opKey}_${key}`}>
-				<div className="title">{key}</div>
-				<div className="value">
-					{key !== 'logs' ? value : this.renderContractLogs(value)}
-					{key === 'bytecode' ? <button className="copy-bytecode" onClick={() => copy(value)}>Copy</button> : null}
-				</div>
-			</div>
+			<React.Fragment>
+				{ bytecode }
+				{
+					value.length > BYTECODE_SYMBOLS_LENGTH && !loadMore[index] ?
+						<a href="" className="load-more" onClick={(e) => this.onToggleLoadMore(index, e)}>
+							{loadMore[index] ? 'Collapse All' : 'Show All'}
+						</a> : null
+				}
+				<button className="copy-bytecode" onClick={() => copy(value)}>Copy</button>
+			</React.Fragment>
 		);
 	}
 
@@ -82,10 +120,27 @@ class TransactionsInfo extends React.Component {
 
 		return (
 			<div className="transaction-info-table" key={opKey}>
-				{
-					Object.entries(operation)
-						.map(([key, value]) => this.renderOperationRow(key, value, opKey))
-				}
+				<div className="operation-title">
+					<span className="gray">operation {index + 1}:</span>&nbsp;{operation.type}
+				</div>
+				<div className="table-help-container">
+					{Object.entries(operation).map(([key, value]) => (
+						<div
+							className={
+								classnames(
+									'row',
+									{ bytecode: ['bytecode', 'logs'].includes(key) },
+								)
+							}
+							key={`${opKey}_${key}`}
+						>
+							<div className="title">{key}</div>
+							<div className="value">
+								{this.renderOperationRowValue(key, value, index)}
+							</div>
+						</div>
+					))}
+				</div>
 			</div>
 		);
 	}
