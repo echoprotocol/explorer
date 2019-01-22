d3704b8e9487f18bd44674c3b28db3f3cf7cc751
Mikhail Shautsou <m.shautsou@pixelplex.io>
2019-01-22 16:48:37 +0300

    Update src/actions/BlockActions.js

--- src/actions/BlockActions.js
+++ src/actions/BlockActions.js
@@ -216,7 +216,7 @@ export const getBlockInformation = (round) => async (dispatch, getState) => {
 			 ${FormatHelper.formatByteSize(handledBlock.get('weight'))}`;
 			value.blockNumber = handledBlock.get('blockNumber');
 		} else {
-			value.reward = '10 ECHO';
+			value.reward = '0 ECHO';
 			const weight = JSON.stringify(planeBlock).length;
 			value.size = `${FormatHelper.formatBlockSize(weight)} ${FormatHelper.formatByteSize(weight)}`;
 			value.blockNumber = FormatHelper.formatAmount(planeBlock.round, 0);
