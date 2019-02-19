fba7b93c501bc51884e177402e8cdbc93d46dae7
Nikita P <thystruby@gmail.com>
2019-02-19 17:14:33 +0300

    Bad internet screen

--- src/components/ErrorScreen/index.jsx
+++ src/components/ErrorScreen/index.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Logotype from '../../components/Logotype';
+import { INDEX_PATH } from '../../constants/RouterConstants';
+
 class ErrorScreen extends React.Component {
 
 	render() {
 
-		const { error } = this.props;
+		// const { error } = this.props;
 
 		return (
-			<div className="f-h-loader">
-				<span className="txt">{error}</span>
+			<div className="not-found-page bad-internet">
+				<div className="logo-container"><Logotype onClick={() => this.props.history.push(INDEX_PATH)} /></div>
+				<div className="container">
+					<div className="img" />
+					<div className="txt-block">
+						<div className="title">Couldn&#39;t reach server or bad internet access</div>
+						<button className="n-f-button" onClick={() => this.props.history.push(INDEX_PATH)}>TRY AGAIN</button>
+					</div>
+				</div>
 			</div>
 		);
 	}
@@ -17,7 +27,8 @@ class ErrorScreen extends React.Component {
 }
 
 ErrorScreen.propTypes = {
-	error: PropTypes.string.isRequired,
+	// error: PropTypes.string.isRequired,
+	history: PropTypes.object.isRequired,
 };
 
 export default ErrorScreen;
---
+++ src/containers/ErrorScreen/index.jsx
@@ -0,0 +1,5 @@
+import { withRouter } from 'react-router';
+
+import ErrorScreen from '../../components/ErrorScreen';
+
+export default withRouter((ErrorScreen));
