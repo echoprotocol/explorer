2dbcf7a01b103faa31dfc1d8901ae9078b41f328
Andrey Fedorovich <a.fedorovich@pixelplex.io>
2019-02-20 16:34:10 +0300

    EEX-368 add reload when not internet connection

--- src/components/ErrorScreen/index.jsx
+++ src/components/ErrorScreen/index.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import Logotype from '../../components/Logotype';
-import { INDEX_PATH } from '../../constants/RouterConstants';
 
 class ErrorScreen extends React.Component {
 
+	reload() {
+		window.location.reload();
+	}
+
+
 	render() {
-
-		// const { error } = this.props;
-
+		const { error } = this.props;
 		return (
 			<div className="not-found-page bad-internet">
-				<div className="logo-container"><Logotype onClick={() => this.props.history.push(INDEX_PATH)} /></div>
+				<div className="logo-container"><Logotype onClick={() => this.reload()} /></div>
+
+
 				<div className="container">
 					<div className="img" />
 					<div className="txt-block">
-						<div className="title">Couldn&#39;t reach server or bad internet access</div>
-						<button className="n-f-button" onClick={() => this.props.history.push(INDEX_PATH)}>TRY AGAIN</button>
+						<div className="title">{ error }</div>
+						<button className="n-f-button" onClick={() => this.reload()}>TRY AGAIN</button>
 					</div>
 				</div>
 			</div>
@@ -27,8 +30,7 @@ class ErrorScreen extends React.Component {
 }
 
 ErrorScreen.propTypes = {
-	// error: PropTypes.string.isRequired,
-	history: PropTypes.object.isRequired,
+	error: PropTypes.string.isRequired,
 };
 
 export default ErrorScreen;
