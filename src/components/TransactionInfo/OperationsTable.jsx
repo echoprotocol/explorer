492bbeba45762a92820ddc3a37e20c2faf5ece8f
maxshev <max_shev@list.ru>
2019-09-23 10:48:42 +0300

    [EEX-581] Fix lint errors

--- src/components/TransactionInfo/OperationsTable.jsx
+++ src/components/TransactionInfo/OperationsTable.jsx
@@ -100,7 +100,7 @@ class OperationsTable extends React.Component {
 		const { showedOperations, airRows } = this.state;
 
 		return (
-			<div className={classnames('accordion-table-wrap', {'table-contract': !isTransaction}, { 'table-block': isBlock })} >
+			<div className={classnames('accordion-table-wrap', { 'table-contract': !isTransaction }, { 'table-block': isBlock })} >
 				<table>
 					<Media query="(max-width: 767px)">
 						{ (matches) => !matches &&
