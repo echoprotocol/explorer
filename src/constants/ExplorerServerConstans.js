004e9c2ad9fea741dd19276d47ce1562efba5347
Alina Lozyuk <32218750+AlinaLoz@users.noreply.github.com>
2019-10-14 14:23:31 +0300

    [EEX-616] fix files with constants (#27)

--- src/constants/ExplorerServerConstans.js
+++ src/constants/ExplorerServerConstans.js
@@ -4,10 +4,3 @@ export const VERIFY_PREFIX = 'verify';
 export const API_PREFIX = 'api';
 export const ABI_PREFIX = 'abi';
 export const SEARCH_PREFIX = 'search';
-
-export const ACCOUNT_PREFIX = 'accounts';
-export const AVATAR_PREFIX = 'avatar.png';
-
-export const OG_TITLE_REGEX = /OG_TITLE/;
-export const OG_DESCRIPTION_REGEX = /OG_DESCRIPTION/;
-export const OG_IMAGE_REGEX = /OG_IMAGE/;
--- src/constants/GraphqlConstans.js
+++ src/constants/GraphqlConstans.js
@@ -1,7 +1,2 @@
 export const OPERATION_DEFINITION = 'OperationDefinition';
 export const SUBSCRIPTION = 'subscription';
-
-export const ASSET_TYPE = 'ASSET';
-export const TOKEN_TYPE = 'TOKEN';
-
-export const DEFAULT_HISTORY_COUNT = 30;
