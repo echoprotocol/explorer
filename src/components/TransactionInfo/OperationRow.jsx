c9b5ddfc36ed4ab6facfa6c7882056c319a66e36
Anastasia Rudenko <a.rudenko@pixelplex.io>
2019-11-04 13:17:12 +0300

    [EEX-653] init block preprating, fix time and block number

--- src/components/TransactionInfo/OperationRow.jsx
+++ src/components/TransactionInfo/OperationRow.jsx
@@ -189,7 +189,7 @@ class OperationRow extends React.Component {
 						{
 							mainInfo.from.name ? (
 								<Media
-									queries={{
+									query={{
 										small: '(max-width: 767px)',
 										large: '(min-width: 768px)',
 									}}
