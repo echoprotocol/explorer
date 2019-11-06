68dafbfc3d516fd9ca686e26dba13f5b8130357c
maxshev <31995360+maxshev@users.noreply.github.com>
2019-11-06 18:16:40 +0300

    [EEX-654] Added node map page (#57)
    * [EEX-604] Added node map page
    
    * Fixed issues after review
    
    * Fixed issues after review

---
+++ src/components/Header/NavTabs.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { withRouter } from 'react-router';
+
+import { INDEX_PATH, NODE_MAP } from '../../constants/RouterConstants';
+
+class NavTabs extends React.Component {
+
+	render() {
+		const { pathName, history } = this.props;
+
+		return (
+			<div className="navigation-tabs">
+				<button
+					className={classnames({ active: pathName === INDEX_PATH })}
+					onClick={() => history.push(INDEX_PATH)}
+				>
+					Blocks
+				</button>
+				<button
+					className={classnames({ active: pathName === NODE_MAP })}
+					onClick={() => history.push(NODE_MAP)}
+				>
+					Nodes map
+				</button>
+			</div>
+		);
+	}
+
+}
+
+NavTabs.propTypes = {
+	pathName: PropTypes.string.isRequired,
+	history: PropTypes.object,
+};
+NavTabs.defaultProps = {
+	history: {},
+};
+export default withRouter(connect(
+	(state) => ({
+		pathName: state.router.location.pathname,
+	}),
+	() => ({}),
+)(NavTabs));
