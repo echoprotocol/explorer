20948a95bc9af8be3a25d169a851ffef6e27ce00
maxshev <m.shevchenko@pixelplex.io>
2019-11-13 10:04:37 +0300

    Fixed lint issue

--- src/components/ManageContract/index.jsx
+++ src/components/ManageContract/index.jsx
@@ -111,7 +111,14 @@ class ManageContract extends React.Component {
 							Before updating contract info you need to verify your authority via {' '}
 							</span>
 							<span className="no-wrap">
-								<a href="https://chrome.google.com/webstore/detail/echo-bridge/ginklfodpcgldnicehmlpehfmgjhbdcl" className="link" target="_blank">Bridge</a>{' '}
+								<a
+									href="https://chrome.google.com/webstore/detail/echo-bridge/ginklfodpcgldnicehmlpehfmgjhbdcl"
+									className="link"
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									Bridge
+								</a>{' '}
 								<span className="bridge-logo">
 									<img src={bridge} alt="" />
 								</span>
