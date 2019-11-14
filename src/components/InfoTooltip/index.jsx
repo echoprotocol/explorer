99009c9d5d9807ddddbf7fc79a606e95abcd7a00
maxshev <31995360+maxshev@users.noreply.github.com>
2019-11-14 16:53:54 +0300

    WIP: [EEX-655] Added reward table (#65)
    * [EEX-655] Added reward table
    
    * Fixed issues
    
    * Hide reward table

---
+++ src/components/InfoTooltip/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Tooltip from 'rc-tooltip';
+import PropTypes from 'prop-types';
+
+import infoIcon from '../../assets/images/icons/info-icn.svg';
+import infoHoverIcon from '../../assets/images/icons/info-hover.svg';
+
+class InfoTooltip extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			hovered: false,
+		};
+	}
+
+	changeHover(value) {
+		this.setState({ hovered: value });
+	}
+
+	render() {
+
+		const { hovered } = this.state;
+		const { tooltipText	} = this.props;
+
+		return (
+			<Tooltip
+				placement="rightBottom"
+				trigger={['hover']}
+				overlay={<span>{tooltipText}</span>}
+				onMouseEnter={() => this.changeHover(true)}
+				onMouseLeave={() => this.changeHover(false)}
+			>
+				<span className="tooltip">
+					<img src={hovered ? infoHoverIcon : infoIcon} alt="" />
+				</span>
+			</Tooltip>
+
+		);
+	}
+
+}
+
+InfoTooltip.propTypes = {
+	tooltipText: PropTypes.string,
+};
+
+InfoTooltip.defaultProps = {
+	tooltipText: '',
+};
+
+export default InfoTooltip;
