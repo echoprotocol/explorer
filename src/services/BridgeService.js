35d2a8a663a8618eb8de39dfcd5b9aaefc98b101
kosAndre22 <a.kosobuckiy@pixelplex.io>
2019-11-15 18:00:02 +0300

    fix one window

--- src/services/BridgeService.js
+++ src/services/BridgeService.js
@@ -2,7 +2,7 @@
 export class BridgeService {
 
 	static isExist() {
-		return window.echojslib && window.echojslib.extension;
+		return !!window.echojslib && !!window.echojslib.extension;
 	}
 
 	static subscribeSwitchAccount(cb) {
@@ -21,8 +21,8 @@ export class BridgeService {
 		// window.echojslib.extension.unscribeSwitchAccount(cb);
 	}
 
-	static getAccounts() {
-		return window.echojslib.extension.getAccounts();
+	static getAccount() {
+		return window.echojslib.extension.activeAccount;
 	}
 
 
