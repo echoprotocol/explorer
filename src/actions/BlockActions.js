5d63a7b9e227870de759b555891487e437cef053
mikhail.shautsou <m.shautsou@pixelplex.io>
2019-11-18 16:03:42 +0300

    fix

--- src/actions/BlockActions.js
+++ src/actions/BlockActions.js
@@ -79,6 +79,9 @@ export const getBlockInformation = (round) => async (dispatch, getState) => {
 
 		let resultTransactions = [];
 		if (transactions.length !== 0) {
+			// TODO: didnt work with ops data
+			// const ops = transactions.reduce((resultIds, { operations }) => resultIds.concat(operations), []);
+			// await TransactionActions.fetchTransactionsObjects(ops);
 
 			const promiseTransactions = transactions
 				.map(({ operations, operation_results }, trIndex) =>
