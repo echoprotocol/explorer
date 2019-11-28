c262e4030e577756513b8f03778bd0be18564358
Ales <a.pazniak@pixelplex.io>
2019-11-28 11:27:28 +0200

    [EEX-493] effect on click copy (#99)

--- src/components/Contract/ContractBytecode.jsx
+++ src/components/Contract/ContractBytecode.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import copy from 'copy-to-clipboard';
 
 class ContractBytecode extends React.Component {
 
@@ -14,7 +13,6 @@ class ContractBytecode extends React.Component {
 						{
 							bytecode ?
 								<React.Fragment>
-									<button className="copy-bytecode" onClick={() => copy(bytecode)}>Copy code</button>
 									<div className="text-block">{bytecode}</div>
 								</React.Fragment> :
 								<div className="text-block">None</div>
