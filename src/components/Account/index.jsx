72b7d01f4b4f5e8b1ff93cf6739a2e5d28ffcaf1
aFedorovich <a.fedorovich@pixelplex.io>
2019-12-24 18:28:36 +0300

    m

--- src/components/Account/index.jsx
+++ src/components/Account/index.jsx
@@ -69,7 +69,7 @@ class Account extends React.Component {
 		} = this.props;
 
 		return (
-			<div className="table-container inner-information-container block-information account-page with-d-table">
+			<div className="table-container inner-information-container account-page">
 				<div className="account-page-t-block">
 					{account && <div className="title">Account {account.get('id')}</div>}
 					<div className="help-container">
