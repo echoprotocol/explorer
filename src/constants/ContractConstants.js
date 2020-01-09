9522409773e39351253ada18ab844627c4eaae66
kosAndrey22 <a.kosobuckiy@pixelplex.io>
2020-01-09 18:35:30 +0300

    [EEX-751] Fix large contract verify error (#149)

--- src/constants/ContractConstants.js
+++ src/constants/ContractConstants.js
@@ -29,4 +29,7 @@ export const CONTRACT_TABS = {
 };
 
 export const CHANGE_TEXT_TIME = 2000;
-
+export const COMPILER_CONSTS = {
+	SOLC_NOT_ENOUGH_STACK_ERROR: 'Maximum call stack size exceeded',
+	MAX_TRIES_TO_COMPILE: 2,
+};
