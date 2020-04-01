030c802d4cb9b0c24c9ed2b1deb0f4dfd1f98f33
a.lozyuk <a.lozyuk@pixelplex.io>
2020-04-01 11:59:20 +0300

    fix svg avatar

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import PropTypes from 'prop-types';
-// import { svgAvatar } from 'echojs-ping';
 import classnames from 'classnames';
 
 import avatar from '../../public/images/default-avatar.svg';
 
+const SvgAvatar = dynamic(() => import('./svgAvatar'), {
+	ssr: false,
+});
+
 class Avatar extends React.Component {
 
-	// static updateAccountName(props) {
-	// 	return { accountName: props.accountName };
-	// }
+	static updateAccountName(props) {
+		return { accountName: props.accountName };
+	}
 
 	constructor(props) {
 		super(props);
@@ -19,20 +23,20 @@ class Avatar extends React.Component {
 			accountName: '',
 		};
 		this.imageRef = React.createRef();
-		// this.listener = this.updateAvatarSize.bind(this);
+		this.listener = this.updateAvatarSize.bind(this);
 	}
 
 	componentDidMount() {
-		// this.updateAvatarSize();
+		this.updateAvatarSize();
 		window.addEventListener('resize', this.listener);
 		window.addEventListener('load', this.listener);
 	}
 
 	static getDerivedStateFromProps(nextProps, prevState) {
 		const { accountName } = prevState;
-		// if ((accountName !== nextProps.accountName && nextProps.accountName) || nextProps.reset) {
-		// 	return Avatar.updateAccountName(nextProps);
-		// }
+		if ((accountName !== nextProps.accountName && nextProps.accountName) || nextProps.reset) {
+			return Avatar.updateAccountName(nextProps);
+		}
 		return null;
 	}
 
@@ -48,24 +52,25 @@ class Avatar extends React.Component {
 		window.removeEventListener('resize', this.listener);
 	}
 
-	// updateAvatarSize() {
-	// 	const avatarSize = this.imageRef.current.offsetHeight;
-	// 	if (avatarSize !== this.state.avatarSize) {
-	// 		this.setState({ avatarSize });
-	// 	}
-	// }
+	updateAvatarSize() {
+		const avatarSize = this.imageRef.current.offsetHeight;
+		if (avatarSize !== this.state.avatarSize) {
+			this.setState({ avatarSize });
+		}
+	}
 
 	render() {
 		const { round } = this.props;
 		const { avatarSize, accountName } = this.state;
 
-		// !accountName ? <img src={avatar} alt="avatar" /> : (
-		// 	<div dangerouslySetInnerHTML={{ __html: svgAvatar(accountName, avatarSize) }} />
-		// )
-		//
+
 		return (
 			<div ref={this.imageRef} className={classnames('avatar-image', { round })}>
-				<img src={avatar} alt="avatar" />
+				{
+					!accountName ? <img src={avatar} alt="avatar" /> : (
+						<SvgAvatar accountName={accountName} avatarSize={avatarSize} />
+					)
+				}
 			</div>
 		);
 	}
