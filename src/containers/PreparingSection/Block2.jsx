0c4a3fade0ac94ce80d81e27ee1b15a628fd777e
kosAndre22 <a.kosobuckiy@pixelplex.io>
2020-04-08 16:04:17 +0300

    almost work

--- src/containers/PreparingSection/Block2.jsx
+++ src/containers/PreparingSection/Block2.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { LatestBlockIcon } from '../../components/Icons/HeaderIcons';
 
 
@@ -7,7 +8,8 @@ const Block2 = React.memo((props) => (
 		<div className="preparing-head">
 			<LatestBlockIcon />
 			<span className="preparing-caption">
-				<span className="accent">12h </span> : 33m : 43s
+				<span className="accent">{Math.floor(props.diff / 1000 / 60 / 60)}h </span>
+				: {Math.floor(props.diff / 1000 / 60)}m : {Math.floor(props.diff / 1000)}s
 			</span>
 		</div>
 		<div className="preparing-line">
@@ -15,5 +17,9 @@ const Block2 = React.memo((props) => (
 		</div>
 	</React.Fragment>
 ));
+
+Block2.propTypes = {
+	diff: PropTypes.number.isRequired,
+};
 
 export default Block2;
--- src/containers/PreparingSection/index.jsx
+++ src/containers/PreparingSection/index.jsx
@@ -7,21 +7,36 @@ import Block2 from './Block2';
 import Block3 from './Block3';
 import Block4 from './Block4';
 
-const PreparingSection = () => (
-	<div className="preparing-wrap">
-		<PreparingBlock><Block1 /></PreparingBlock>
-		<PreparingBlock><Block2 /></PreparingBlock>
-		<PreparingBlock><Block3 /></PreparingBlock>
-		<PreparingBlock><Block4 /></PreparingBlock>
-	</div>
-);
+const PreparingSection = (props) => {
+	// const [time, setTime] = useState(0);
+	const { latestBlock, blocks } = props;
+	const lastTimestamp = Date.parse(blocks.getIn([latestBlock, 'timestamp']));
+	const GMT = new Date().getTimezoneOffset();
+	const diff = (Date.now() - lastTimestamp) + (GMT * 60 * 1000);
+	let a = diff;
+	setInterval(() => {
+		console.log(111)
+		a += 1000;
+		console.log(a)
+	}, 1000);
+	return (
+		<div className="preparing-wrap">
+			<PreparingBlock><Block1 /></PreparingBlock>
+			<PreparingBlock><Block2 diff={diff} /></PreparingBlock>
+			<PreparingBlock><Block3 /></PreparingBlock>
+			<PreparingBlock><Block4 /></PreparingBlock>
+		</div>
+	);
+};
 
 PreparingSection.propTypes = {
+	blocks: PropTypes.object.isRequired,
 	latestBlock: PropTypes.number.isRequired,
 };
 
 export default connect(
 	(state) => ({
+		blocks: state.block.get('blocks'),
 		latestBlock: state.round.get('latestBlock'),
 	}),
 	() => ({}),
