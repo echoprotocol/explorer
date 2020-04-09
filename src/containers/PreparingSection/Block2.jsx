81bce44730ed1a601308cc9c440f142dfbae71f3
kosAndre22 <a.kosobuckiy@pixelplex.io>
2020-04-09 14:26:56 +0300

    fix timer

--- src/containers/PreparingSection/Block2.jsx
+++ src/containers/PreparingSection/Block2.jsx
@@ -1,29 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { LatestBlockIcon } from '../../components/Icons/HeaderIcons';
 import Timer from './Timer';
 
-class Block2 extends Component {
-
-	render() {
-		return (
-			<React.Fragment>
-				<div className="preparing-head">
-					<LatestBlockIcon />
-					<span className="preparing-caption">
-						<Timer
-							diff={this.props.diff}
-						/>
-					</span>
-				</div>
-				<div className="preparing-line">
-					<span className="preparing-text">Latest block time</span>
-				</div>
-			</React.Fragment>
-		);
-	}
-
-}
+const Block2 = React.memo((props) => (
+	<React.Fragment>
+		<div className="preparing-head">
+			<LatestBlockIcon />
+			<span className="preparing-caption">
+				<Timer diff={props.diff} />
+			</span>
+		</div>
+		<div className="preparing-line">
+			<span className="preparing-text">Latest block time</span>
+		</div>
+	</React.Fragment>
+));
 
 Block2.propTypes = {
 	diff: PropTypes.number.isRequired,
