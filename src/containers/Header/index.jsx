48b3ac2f9abe661bfcf89255ddd8c7c3900d9ad8
Andrey Kosobutskiy <a.kosobuckiy@pixelplex.io>
2020-04-10 14:58:05 +0300

    [EEX-803] Add logic to echorand block

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -10,6 +10,7 @@ import Header from '../../components/Header';
 
 const HeaderContainer = React.memo(({
 	hints, getHints, loadingSearch, errorSearch,
+	stepProgress, preparingBlock, latestBlock,
 }) => (
 	<div className="top-section">
 		<div className="wrap">
@@ -19,7 +20,11 @@ const HeaderContainer = React.memo(({
 				hints={hints}
 				getHints={getHints}
 			/>
-			<PreparingSection />
+			<PreparingSection
+				stepProgress={stepProgress}
+				preparingBlock={preparingBlock}
+				latestBlock={latestBlock}
+			/>
 		</div>
 	</div>
 ));
@@ -30,6 +35,9 @@ HeaderContainer.propTypes = {
 	loadingSearch: PropTypes.bool.isRequired,
 	errorSearch: PropTypes.string.isRequired,
 	getHints: PropTypes.func.isRequired,
+	stepProgress: PropTypes.string.isRequired,
+	preparingBlock: PropTypes.number.isRequired,
+	latestBlock: PropTypes.number.isRequired,
 };
 
 export default withRouter(connect(
@@ -37,6 +45,9 @@ export default withRouter(connect(
 		hints: state.search.getIn(['headerSearch', 'hints']),
 		errorSearch: state.search.getIn(['headerSearch', 'error']),
 		loadingSearch: state.search.getIn(['headerSearch', 'loading']),
+		stepProgress: state.round.get('stepProgress'),
+		preparingBlock: state.round.get('preparingBlock'),
+		latestBlock: state.round.get('latestBlock'),
 	}),
 	(dispatch) => ({
 		getHints: (str) => dispatch(searchActions.headerSearchHint(str)),
--- src/containers/PreparingSection/index.jsx
+++ src/containers/PreparingSection/index.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import PreparingBlock from './PreparingBlock';
 import Block1 from './Block1';
 import Block2 from './Block2';
 import Block3 from './Block3';
 import Block4 from './Block4';
 
-const PreparingSection = React.memo(() => (
+const PreparingSection = (props) => (
 	<div className="preparing-wrap">
-		<PreparingBlock><Block1 /></PreparingBlock>
+		<PreparingBlock>
+			<Block1
+				stepProgress={props.stepProgress}
+				preparingBlock={props.preparingBlock}
+				latestBlock={props.latestBlock}
+			/>
+		</PreparingBlock>
 		<PreparingBlock><Block2 /></PreparingBlock>
 		<PreparingBlock><Block3 /></PreparingBlock>
 		<PreparingBlock><Block4 /></PreparingBlock>
 	</div>
-));
+);
+
+PreparingSection.propTypes = {
+	stepProgress: PropTypes.string.isRequired,
+	preparingBlock: PropTypes.number.isRequired,
+	latestBlock: PropTypes.number.isRequired,
+};
 
 export default PreparingSection;
