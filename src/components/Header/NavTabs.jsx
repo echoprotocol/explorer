9b9e84855de1514205cd22e6a609d2f35f1a86d1
Andrey Fedorovich <a.fedorovich@pixelplex.io>
2020-04-14 14:44:02 +0300

    [EEX-853] navTabs, fix next active link

--- src/components/Header/NavTabs.jsx
+++ src/components/Header/NavTabs.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import cn from 'classnames';
-import Router, { withRouter } from 'next/router';
-
+import Router, { withRouter, useRouter } from 'next/router';
 import { INDEX_PATH, NODE_MAP } from '../../constants/RouterConstants';
 
-const NavTabs = React.memo(({ router: { pathname } }) => (
-	<div className="nav-tabs">
-		<button
-			className={cn({ active: pathname === INDEX_PATH })}
-			onClick={() => Router.push(INDEX_PATH)}
-		>
-			Blocks
-		</button>
-		<button
-			className={cn({ active: pathname === NODE_MAP })}
-			onClick={() => Router.push(NODE_MAP)}
-		>
-			Nodes map
-		</button>
-	</div>
-));
-
-NavTabs.propTypes = {
-	router: PropTypes.object.isRequired,
-};
-
-NavTabs.defaultProps = {};
+const NavTabs = React.memo(() => {
+	const router = useRouter();
+	return (
+		<div className="nav-tabs">
+			<button
+				className={cn({ active: router.pathname === INDEX_PATH })}
+				onClick={() => Router.push(INDEX_PATH)}
+			>
+				Blocks
+			</button>
+			<button
+				className={cn({ active: router.pathname === NODE_MAP })}
+				onClick={() => Router.push(NODE_MAP)}
+			>
+				Nodes map
+			</button>
+		</div>
+	);
+});
 
 export default withRouter(NavTabs);
