0065144010eabd899bb223714837e5cbc0c4e25d
Sergey <s.kilochytsky@pixelplex.io>
2020-04-16 15:24:49 +0300

    fix view bugs in frozen-funds block

--- src/components/Sidebar/FrozenFunds.jsx
+++ src/components/Sidebar/FrozenFunds.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { LineChart, Line } from 'recharts';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import Tooltip from 'rc-tooltip';
 import { SIDEBAR_CHART_WIDTH, SIDEBAR_CHART_HEIGHT } from '../../constants/UiConstants';
 import FormatHelper from '../../helpers/FormatHelper';
 import URLHelper from '../../helpers/URLHelper';
@@ -15,9 +16,16 @@ const FrozenFunds = ({ currentFrozenData, frozenData }) => (
 				<div className="sidebar-element-block-title">Comitee amount</div>
 				<div className="frozen-funds-block-info">
 					<div className="comitee-amount">
-						<div className="comitee-amount-value">
-							{FormatHelper.formatAmount(currentFrozenData.committee_freeze_sum)}
-						</div>
+						<Tooltip
+							placement="top"
+							overlayClassName="verify-contract-tooltip"
+							trigger={['hover']}
+							overlay={FormatHelper.formatAmount(currentFrozenData.committee_freeze_sum, ECHO_ASSET.PRECISION)}
+						>
+							<div className="comitee-amount-value">
+								{FormatHelper.formatAmount(currentFrozenData.committee_freeze_sum, ECHO_ASSET.PRECISION)}
+							</div>
+						</Tooltip>
 						<div className="comitee-amount-coin">
 							<Link href={SSR_ASSET_PATH} as={URLHelper.createAssetUrl(ECHO_ASSET.ID)}>
 								<a href="" className="total-supply-coin">
@@ -26,7 +34,7 @@ const FrozenFunds = ({ currentFrozenData, frozenData }) => (
 							</Link>
 						</div>
 					</div>
-					<LineChart
+					{/* <LineChart
 						width={SIDEBAR_CHART_WIDTH}
 						height={SIDEBAR_CHART_HEIGHT}
 						data={frozenData}
@@ -38,16 +46,23 @@ const FrozenFunds = ({ currentFrozenData, frozenData }) => (
 							dataKey="committee_freeze_sum"
 							stroke="#2995D8"
 						/>
-					</LineChart>
+					</LineChart> */}
 				</div>
 			</div>
 			<div className="sidebar-element-block">
 				<div className="sidebar-element-block-title">Users amount</div>
 				<div className="frozen-funds-block-info">
 					<div className="users-amount">
-						<div className="users-amount-value">
-							{FormatHelper.formatAmount(currentFrozenData.accounts_freeze_sum)}
-						</div>
+						<Tooltip
+							placement="top"
+							overlayClassName="verify-contract-tooltip"
+							trigger={['hover']}
+							overlay={FormatHelper.formatAmount(currentFrozenData.accounts_freeze_sum, ECHO_ASSET.PRECISION)}
+						>
+							<div className="comitee-amount-value">
+								{FormatHelper.formatAmount(currentFrozenData.accounts_freeze_sum, ECHO_ASSET.PRECISION)}
+							</div>
+						</Tooltip>
 						<div className="users-amount-coin">
 							<Link href={SSR_ASSET_PATH} as={URLHelper.createAssetUrl(ECHO_ASSET.ID)}>
 								<a href="" className="total-supply-coin">
