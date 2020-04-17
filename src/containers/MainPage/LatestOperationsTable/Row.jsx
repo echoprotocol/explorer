06dad820222e01ab9071cf13af9c9bdfee4a7768
Andrey Kosobutskiy <a.kosobuckiy@pixelplex.io>
2020-04-17 13:40:54 +0300

    [EEX] Fix avatar displaying

--- src/containers/MainPage/LatestOperationsTable/Row.jsx
+++ src/containers/MainPage/LatestOperationsTable/Row.jsx
@@ -2,63 +2,66 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import cn from 'classnames';
+import { validators } from 'echojs-lib';
+import Router from 'next/router';
 
 import Avatar from '../../../components/Avatar';
 import URLHelper from '../../../helpers/URLHelper';
-import { SSR_ACCOUNTS_PATH } from '../../../constants/RouterConstants';
 import FormatHelper from '../../../helpers/FormatHelper';
+import SsrHrefHelper from '../../../helpers/SsrHrefHelper';
 
 const OperationsRow = React.memo(({
 	operation, from, to, amount, onClick,
-}) => (
-	<React.Fragment>
-		<tr onClick={onClick} className={cn('view')}>
-			<td className="operation"><div className="td-in">{operation}</div></td>
-			<td className="from">
-				{
-					from ?
-						<Link href={SSR_ACCOUNTS_PATH} as={URLHelper.createAccountUrlByName(from)}>
-							<a className="td-in avatar-wrap">
-								<Avatar accountName={from} />
-								<span>{from}</span>
-							</a>
-						</Link>
-						:
-						<span>-</span>}
-			</td>
-			<td className="to">
-				{
-					to ?
-						<Link href={SSR_ACCOUNTS_PATH} as={URLHelper.createAccountUrlByName(to)}>
-							<a className="td-in avatar-wrap">
-								<Avatar accountName={to} />
-								<span>{to}</span>
-							</a>
-						</Link> :
-						<span>-</span>}
-			</td>
-			<td className="amount">
-				<div className="td-in">
-					<span className="value">{FormatHelper.formatAmount(amount.value, amount.precision)}</span>
-					<span className="type">{amount.coin}</span>
-				</div>
-			</td>
-		</tr>
-	</React.Fragment>
-));
+}) => {
+
+	const goToSubject = (e, url, id) => {
+		e.preventDefault();
+		e.stopPropagation();
+		Router.push(SsrHrefHelper.getHrefByObjectId(id), url);
+	};
+
+	const renderSubject = (subject) => {
+		if (typeof subject === 'object' && !subject.name && !subject.id) {
+			return <div className="td-in">—</div>;
+		}
+		if (validators.isHex(subject) && subject.length === 40) return <span className="td-in"><span>{subject}</span></span>;
+		return (
+			<Link href={SsrHrefHelper.getHrefByObjectId(subject.id)} as={URLHelper.createUrlById(subject.id)}>
+				<a href="" className="td-in avatar-wrap" onClick={(e) => goToSubject(e, URLHelper.createUrlById(subject.id), subject.id)}>
+					{validators.isAccountId(subject.id) && <Avatar accountName={subject.name} />}
+					<span>{subject.name ? subject.name : subject.id}</span>
+				</a>
+			</Link>
+		);
+	};
+
+	return (
+		<React.Fragment>
+			<tr onClick={onClick} className={cn('view')}>
+				<td className="operation"><div className="td-in">{operation}</div></td>
+				<td className="from">
+					{renderSubject(from)}
+				</td>
+				<td className="to">
+					{renderSubject(to)}
+				</td>
+				<td className="amount">
+					<div className="td-in">
+						<span className="value">{FormatHelper.formatAmount(amount.value, amount.precision)}</span>
+						<span className="type">{amount.coin}</span>
+					</div>
+				</td>
+			</tr>
+		</React.Fragment>
+	);
+});
 
 OperationsRow.propTypes = {
 	operation: PropTypes.string.isRequired,
-	from: PropTypes.string,
-	to: PropTypes.string,
+	from: PropTypes.object.isRequired,
+	to: PropTypes.object.isRequired,
 	amount: PropTypes.object.isRequired,
 	onClick: PropTypes.func.isRequired,
 };
-
-OperationsRow.defaultProps = {
-	from: '',
-	to: '',
-};
-
 
 export default OperationsRow;
