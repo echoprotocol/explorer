f29439ce192b2730f3ac45c0a89f27d4440c2b71
Alina Lozyuk <a.lozyuk@pixelplex.io>
2020-04-17 10:40:17 +0300

    [EEX-864] fix producer link in latest blocks

--- src/containers/MainPage/LatestBlocksTable/Row.jsx
+++ src/containers/MainPage/LatestBlocksTable/Row.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
+import Router from 'next/router';
 import cn from 'classnames';
 
 import Avatar from '../../../components/Avatar';
@@ -9,29 +10,38 @@ import { SSR_ACCOUNTS_PATH } from '../../../constants/RouterConstants';
 
 const OperationsRow = React.memo(({
 	number, age, producer, size, txs, onClick, date,
-}) => (
-	<React.Fragment>
-		<tr onClick={onClick} className={cn('view')}>
-			<td className="number"><div className="td-in">{number}.</div></td>
-			<td className="age">
-				<div className="td-in">
-					<span className="age-hint">{date},</span>
-					<span className="age-value">{age}</span>
-				</div>
-			</td>
-			<td className="producer">
-				<Link href={SSR_ACCOUNTS_PATH} as={URLHelper.createAccountUrlByName(producer)}>
-					<a className="td-in avatar-wrap">
-						<Avatar accountName={producer} />
-						<span>{producer}</span>
-					</a>
-				</Link>
-			</td>
-			<td className="size"><div className="td-in">{size.weight} {size.weightSize}</div></td>
-			<td className="txs"><div className="td-in">{txs}</div></td>
-		</tr>
-	</React.Fragment>
-));
+}) => {
+	const producerLink = URLHelper.createAccountUrlByName(producer);
+	const goToAccount = (e) => {
+		e.preventDefault();
+		e.stopPropagation();
+		Router.push(SSR_ACCOUNTS_PATH, producerLink);
+	};
+
+	return (
+		<React.Fragment>
+			<tr onClick={onClick} className={cn('view')}>
+				<td className="number"><div className="td-in">{number}.</div></td>
+				<td className="age">
+					<div className="td-in">
+						<span className="age-hint">{date},</span>
+						<span className="age-value">{age}</span>
+					</div>
+				</td>
+				<td className="producer">
+					<Link href={SSR_ACCOUNTS_PATH} as={producerLink}>
+						<a href={producerLink} className="td-in avatar-wrap" onClick={(e) => goToAccount(e)}>
+							<Avatar accountName={producer} />
+							<span>{producer}</span>
+						</a>
+					</Link>
+				</td>
+				<td className="size"><div className="td-in">{size.weight} {size.weightSize}</div></td>
+				<td className="txs"><div className="td-in">{txs}</div></td>
+			</tr>
+		</React.Fragment>
+	);
+});
 
 OperationsRow.propTypes = {
 	number: PropTypes.string.isRequired,
