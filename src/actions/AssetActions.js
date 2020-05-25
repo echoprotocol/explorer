1b8779533ef47e57dd2f9bdd9b5527f6e9de7b08
kosAndre22 <a.kosobuckiy@pixelplex.io>
2020-05-25 15:22:25 +0300

    add asset info block

--- src/actions/AssetActions.js
+++ src/actions/AssetActions.js
@@ -1,6 +1,7 @@
 import echo from 'echojs-lib';
 
 import GlobalActions from './GlobalActions';
+import { getAssetFlags } from '../services/transform.ops/AddInfoHelper';
 
 /**
  * @method getFullAssetInformation
@@ -17,6 +18,11 @@ export const getFullAssetInformation = (assetId) => async (dispatch) => {
 		}
 
 		const issuer = await echo.api.getObject(asset.issuer);
+
+		const issuerPermissions = asset && asset.options.issuer_permissions;
+		const flags = asset && asset.options.flags;
+		const flagsValues = getAssetFlags({ flags, issuer_permissions: issuerPermissions });
+		asset.options.flags = Object.entries(flagsValues).map((el) => ({ key: el[0], value: el[1] }));
 
 		// eslint-disable-next-line consistent-return
 		return { asset, issuer };
