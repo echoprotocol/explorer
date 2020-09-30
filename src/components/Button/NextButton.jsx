5f0236702449fc0ab870ad7babd3afc335f67a3d
Roman Krutsko <r.krutko@pixelplex.io>
2020-09-30 16:04:55 +0300

    [EEX-1081] reverse btns text & show toggle arrow

--- src/components/Button/NextButton.jsx
+++ src/components/Button/NextButton.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const NextButton = React.memo(() => (
+export const NextButton = React.memo(({ reverse }) => (
 	<div className="pg-arrow">
-		<div className="pg-arrow-caption">Next</div>
+		<div className="pg-arrow-caption">{reverse ? 'Previous' : 'Next'}</div>
 		<svg width="4" height="5" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path d="M4 2.5L0 5V0l4 2.5z" />
 		</svg>
 	</div>
 ));
+
+NextButton.propTypes = {
+	reverse: PropTypes.bool,
+};
+
+NextButton.defaultProps = {
+	reverse: false,
+};
--- src/components/Button/PrevButton.jsx
+++ src/components/Button/PrevButton.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-export const PrevButton = React.memo(() => (
+export const PrevButton = React.memo(({ reverse }) => (
 	<div className="pg-arrow">
 		<svg width="4" height="5" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path d="M4 2.5L0 5V0l4 2.5z" />
 		</svg>
-		<div className="pg-arrow-caption">Previous</div>
+		<div className="pg-arrow-caption">{reverse ? 'Next' : 'Previous'}</div>
 	</div>
 ));
+
+PrevButton.propTypes = {
+	reverse: PropTypes.bool,
+};
+
+PrevButton.defaultProps = {
+	reverse: false,
+};
