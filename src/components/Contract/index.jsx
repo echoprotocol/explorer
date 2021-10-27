3d29f9d81e6395bacbc94bf22aefe11c288375a0
kosAndre22 <a.kosobuckiy@pixelplex.io>
2021-10-27 14:38:38 +0300

    [HOTFIX] Fix stars displaying

--- src/components/Contract/index.jsx
+++ src/components/Contract/index.jsx
@@ -239,7 +239,7 @@ class Contract extends React.Component {
 								<ActionButton
 									name={stars.includes(activeAccount.get('id')) ? 'Unstar' : 'Star'}
 									onClick={() => this.props.setStarToContract(id)}
-									label={stars.size}
+									label={stars.size > 0 ? stars.size : ''}
 									icon={starIcon}
 								/>
 								<ActionButton name="Edit info" onClick={this.manageContract} icon={editIcon} />
